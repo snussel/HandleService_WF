031ac3fa0610d2d4c22d6313f7842ac01b9a1854
Steven Nussel <Durendal@hotmail.com>
2020-01-30 19:07:46 -0500

    Add project files.

---
+++ HandleService_WF/frmMain.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace HandleService_WF
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvServices = new System.Windows.Forms.DataGridView();
+            this.cmdOn = new System.Windows.Forms.Button();
+            this.cmdOff = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.bwWorker = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvServices
+            // 
+            this.dgvServices.AllowUserToAddRows = false;
+            this.dgvServices.AllowUserToDeleteRows = false;
+            this.dgvServices.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvServices.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvServices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvServices.Location = new System.Drawing.Point(12, 12);
+            this.dgvServices.Name = "dgvServices";
+            this.dgvServices.ReadOnly = true;
+            this.dgvServices.RowHeadersVisible = false;
+            this.dgvServices.RowHeadersWidth = 62;
+            this.dgvServices.RowTemplate.Height = 28;
+            this.dgvServices.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvServices.Size = new System.Drawing.Size(776, 368);
+            this.dgvServices.TabIndex = 0;
+            // 
+            // cmdOn
+            // 
+            this.cmdOn.Enabled = false;
+            this.cmdOn.Location = new System.Drawing.Point(12, 408);
+            this.cmdOn.Name = "cmdOn";
+            this.cmdOn.Size = new System.Drawing.Size(75, 30);
+            this.cmdOn.TabIndex = 2;
+            this.cmdOn.Text = "On";
+            this.cmdOn.UseVisualStyleBackColor = true;
+            this.cmdOn.Click += new System.EventHandler(this.cmdOn_Click);
+            // 
+            // cmdOff
+            // 
+            this.cmdOff.Enabled = false;
+            this.cmdOff.Location = new System.Drawing.Point(93, 407);
+            this.cmdOff.Name = "cmdOff";
+            this.cmdOff.Size = new System.Drawing.Size(75, 30);
+            this.cmdOff.TabIndex = 3;
+            this.cmdOff.Text = "Off";
+            this.cmdOff.UseVisualStyleBackColor = true;
+            this.cmdOff.Click += new System.EventHandler(this.cmdOff_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(712, 407);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 30);
+            this.cmdExit.TabIndex = 4;
+            this.cmdExit.Text = "Exit";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStatus.Location = new System.Drawing.Point(174, 407);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(532, 30);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // bwWorker
+            // 
+            this.bwWorker.WorkerReportsProgress = true;
+            this.bwWorker.WorkerSupportsCancellation = true;
+            this.bwWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwWorker_DoWork);
+            this.bwWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwWorker_ProgressChanged);
+            this.bwWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwWorker_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdOff);
+            this.Controls.Add(this.cmdOn);
+            this.Controls.Add(this.dgvServices);
+            this.Name = "frmMain";
+            this.Text = "Service Handler";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvServices)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvServices;
+        private System.Windows.Forms.Button cmdOn;
+        private System.Windows.Forms.Button cmdOff;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.Label lblStatus;
+        private System.ComponentModel.BackgroundWorker bwWorker;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HandleService_WF/frmMain.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.ServiceModel;
+using System.Xml.Serialization;
+using System.Xml;
+using System.ServiceProcess;
+
+namespace HandleService_WF
+{
+    public partial class frmMain : Form
+    {
+        private static List<ServiceAttributes> ListOfServices = new List<ServiceAttributes>();
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //Enable the timer
+            timer1.Enabled = true;
+        }
+
+        #region Configuration
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            LoadXMLFile();
+
+        }
+
+        private void LoadXMLFile()
+        {
+            //
+            FileInfo confFile = new FileInfo(ConfigurationManager.AppSettings["ServiceFile"]);
+
+            if(confFile.Exists)
+            {
+                lblStatus.Text = "List of services is found";
+                cmdOn.Enabled = false;
+                cmdOff.Enabled = false;
+
+
+                XmlSerializer ser = new XmlSerializer(typeof(Servers));
+                Servers sList;
+                using (XmlReader reader = XmlReader.Create(confFile.FullName))
+                    sList = (Servers)ser.Deserialize(reader);
+
+                foreach(var currentServer in sList.Items )
+                {                    
+                    foreach (var currentService in currentServer.Service)
+                    {
+                        ServiceAttributes t = new ServiceAttributes();
+                        t.ServerName = currentServer.Name;
+                        t.ServiceName = currentService.Value;
+                        t.ServiceStatus = "";
+
+                        ListOfServices.Add(t);
+                    }
+                }
+
+                dgvServices.DataSource = ListOfServices;
+
+                lblStatus.Text = "Updating Services";
+                bwWorker.RunWorkerAsync('U');
+            }
+            else
+            {
+                lblStatus.Text = "No list of services found";
+                cmdOn.Enabled = false;
+                cmdOff.Enabled = false;
+            }
+        }
+        #endregion
+
+        #region Worker
+        private void bwWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            switch(e.Argument)
+            {
+                case 'U':
+                    break;
+                case 'N':
+                    break;
+                case 'F':
+                    break;
+                default:
+                    break;
+            }
+
+            int numRows = dgvServices.Rows.Count;
+
+            for(int i = 0; i < dgvServices.Rows.Count; i++)
+            {                   
+                bwWorker.ReportProgress(i, dgvServices.Rows[i].Cells[0]);
+
+                ServiceController mc = new ServiceController(ListOfServices[i].ServiceName, ListOfServices[i].ServerName);
+
+                try
+                {
+
+                    ListOfServices[i].ServiceStatus = mc.Status.ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.Trim(), "Nope", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                mc.Close();
+
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+
+        private void bwWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.dgvServices.FirstDisplayedScrollingRowIndex = e.ProgressPercentage;
+            lblStatus.Text = $"Updating Service: {e.UserState}";
+        }
+
+        private void bwWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            cmdOff.Enabled = true;
+            cmdOn.Enabled = true;
+            lblStatus.Text = "Complete";
+        }
+        #endregion
+
+        #region Buttons
+        private void cmdOn_Click(object sender, EventArgs e)
+        {
+            cmdOff.Enabled = false;
+            cmdOn.Enabled = false;
+            this.bwWorker.RunWorkerAsync('N');
+        }
+
+        private void cmdOff_Click(object sender, EventArgs e)
+        {
+            cmdOff.Enabled = false;
+            cmdOn.Enabled = false;
+            this.bwWorker.RunWorkerAsync('F');
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            if (bwWorker.IsBusy)
+                this.bwWorker.CancelAsync();
+
+            Application.Exit();
+        }
+        #endregion
+    }
+
+    #region Classes
+    class ServiceAttributes
+    {
+        public string ServerName { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceStatus { get; set; }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class Servers
+    {
+
+        private ServersServer[] itemsField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Server", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
+        public ServersServer[] Items
+        {
+            get
+            {
+                return this.itemsField;
+            }
+            set
+            {
+                this.itemsField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class ServersServer
+    {
+
+        private ServersServerService[] serviceField;
+
+        private string nameField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Service", Form = System.Xml.Schema.XmlSchemaForm.Unqualified, IsNullable = true)]
+        public ServersServerService[] Service
+        {
+            get
+            {
+                return this.serviceField;
+            }
+            set
+            {
+                this.serviceField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string Name
+        {
+            get
+            {
+                return this.nameField;
+            }
+            set
+            {
+                this.nameField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.8.3928.0")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class ServersServerService
+    {
+
+        private string valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+    #endregion
+}
